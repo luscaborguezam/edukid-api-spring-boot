cd8d4a7807e5adc836de2f14a1d8a8ed56a49019
Lucas Borguezam <lucasborguezam@gmail.com>
2024-03-18 09:33:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tg-app-for-time-management-and-academic-stimulation
+ App for time management and academic stimulation with spring boot and react native
---
+++ src/main/java/br/com/learningtime/TgAppForTimeManagementAndAcademicStimulationApplication.java
@@ -0,0 +1,13 @@
+package br.com.learningtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgAppForTimeManagementAndAcademicStimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgAppForTimeManagementAndAcademicStimulationApplication.class, args);
+	}
+
+}
